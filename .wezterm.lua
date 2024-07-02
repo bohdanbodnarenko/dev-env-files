64d91ae0d64e28a0e5183636cc6b1a43cf11fb39
Bohdan Bodnarenko <bohdan.bodnarenko@vista.com>
2024-07-02 15:55:49 +0300

    changed wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,32 +1,37 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+return {
+	-- color_scheme = 'termnial.sexy',
+	color_scheme = "Gruvbox Dark (Gogh)",
+	enable_tab_bar = false,
+	font_size = 16.0,
+	font = wezterm.font("JetBrains Mono"),
+	-- macos_window_background_blur = 40,
+	macos_window_background_blur = 30,
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#706b4e",
-	selection_fg = "#f3d9c4",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
+	-- window_background_image_hsb = {
+	-- 	brightness = 0.01,
+	-- 	hue = 1.0,
+	-- 	saturation = 0.5,
+	-- },
+	-- window_background_opacity = 0.92,
+	window_background_opacity = 1.0,
+	-- window_background_opacity = 0.78,
+	-- window_background_opacity = 0.20,
+	window_decorations = "RESIZE",
+	keys = {
+		{
+			key = "f",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
 }
-
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
-
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 8
-
--- and finally, return the configuration to wezterm
-return config
